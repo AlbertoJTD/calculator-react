dd7bd4abeba01cd44f3ee72bde7955602e91b070
AlbertoJTD <albertojtd@outlook.com>
2022-06-07 16:52:47 -0500

    Added clear button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import logo from './images/spiderman.png'
 import Button from './components/Button'
+import Display from './components/Display.jsx'
+import ClearButton from './components/ClearButton';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         Spider - Calculator 🕸
       </div>
       <div className="calculator-container">
+        <Display />
         <div className="row">
           <Button>7</Button>
           <Button>8</Button>
@@ -26,7 +29,7 @@ function App() {
           <Button>1</Button>
           <Button>2</Button>
           <Button>3</Button>
-          <Button>*</Button>
+          <Button>x</Button>
         </div>
         <div className="row">
           <Button>=</Button>
@@ -35,7 +38,7 @@ function App() {
           <Button>/</Button>
         </div>
         <div className="row">
-          
+          <ClearButton>Clear</ClearButton>
         </div>
       </div>
     </div>
