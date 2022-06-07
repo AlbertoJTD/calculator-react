6048fdfc1ed3306282957e12e8f132e59f77143c
AlbertoJTD <albertojtd@outlook.com>
2022-06-06 21:33:43 -0500

    Added buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import '../styles/Button.css'
 
 function Button(props) {
+
+  const isOperation = (value) => {
+    return isNaN(value) && (value != '.') && (value != '=')
+  }
+
   return (
-    <div>
+    <div className={ `button-container ${isOperation(props.children) ? 'operation' : '' }`.trimEnd() }>
       { props.children }
     </div>
   );
 }
 
-export default Button();
+export default Button;
